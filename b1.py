a989a7ec8a2064ce9c54d75f55a96f811f803b42
Abubakar Bin Danish <abubakrdanish1@gmail.com>
2024-07-03 12:08:18 +0500

    Final Compression file

--- b1.py
+++ b1.py
@@ -1,14 +1,23 @@
-import PySimpleGUI as sg
-
+import FreeSimpleGUI as sg
+import zip_creator
 label1 = sg.Text("Select files to compress: ")
 input1 = sg.Input()
-choose_button1 =sg.FilesBrowse("Choose")
+choose_button1 =sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("Select Destination Folder: ")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
 compress = sg.Button("Compress")
-window = sg.Window("Files to compress",label[[label1,input1,choose_button1],[label2,input2,choose_button2],[compress]])
+output = sg.Text(key="output")
+window = sg.Window("Files to compress",layout=[[label1,input1,choose_button1],[label2,input2,choose_button2],[compress,output]])
 
-window.read()
+
+while True:
+
+    event, values = window.read()
+    print(event, values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    zip_creator.make_archive(filepaths,folder)
+    window["output"].update(value = "Compression Completed!")
 window.close()
