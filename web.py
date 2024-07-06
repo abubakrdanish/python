502302cf1488a4a603c39e8dfb9dc64964d81328
abubakardanish <abubakrdanish1@gmail.com>
2024-07-06 10:03:40 +0500

    implementation of todos app to through streamlit

--- web.py
+++ web.py
@@ -13,9 +13,14 @@ st.title("My Todo App")
 st.subheader("This is a todo app")
 st.write("This app is to increase your productivity.")
 
-# Display existing todos
-for todo in todos:
-    st.checkbox(todo)
 
-# Input for adding a new todo
+for index , todo in enumerate(todos):
+    checkbox=st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
 st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
