f8fdcfdc3f26d2d3044e4bed3c0629d9dce246f3
Abubakar Bin Danish <55455591+abubakrdanish@users.noreply.github.com>
2024-07-12 20:25:14 +0500

    Add files via upload

---
+++ Astronomy Image/main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+
+url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+request = requests.get(url)
+content = request.json()
+
+
+if request.status_code == 200:
+    st.title(content["title"])
+    st.image(content["hdurl"])
+    # Display the explanation in bold and on a new line
+    st.markdown("<br>", unsafe_allow_html=True)  # Add a line break
+    st.markdown(f"<b>{content['explanation']}</b>", unsafe_allow_html=True)
+else:
+    st.write("Failed to retrieve image")
