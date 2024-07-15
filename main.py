0ad99ed52425b3e0442a1c73182f4c24ed70f25f
abubakardanish <abubakrdanish016@gmail.com>
2024-07-15 20:13:16 +0500

    Day 32: Add widgets

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import streamlit as st
 
 st.title("Weather Forecast for the Next Days")
@@ -7,23 +7,3 @@ days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the num
 option = st.selectbox("Select data to view",("Temperature","Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
-=======
-import FreeSimpleGUI as sg
- 
-label = sg.Text("What are dolphins?")
-option1 = sg.Radio("Amphibians", group_id="question1")
-option2 = sg.Radio("Fish", group_id="question1")
-option3 = sg.Radio("Mammals", group_id="question1")
-option4 = sg.Radio("Birds", group_id="question1")
- 
-window = sg.Window("File Compressor",
-                   layout=[[label],
-                           [option1], 
-                           [ option2], 
-                           [option3], 
-                            [option4]
-                           ])
- 
-window.read()
-window.close()
->>>>>>> dfbc9568d53905e9b64b7600f2f197ff90da0bd5
